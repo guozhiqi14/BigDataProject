1e945b7d51eecebabfb4c4d2636e01d2758dbae0
Liwei Song <ls4408@nyu.edu>
2017-04-15 01:41:46 -0400

    map_reduce for 16-23

---
+++ mr_final/crime_18.py
@@ -0,0 +1,21 @@
+from __future__ import print_function
+
+import sys
+from operator import add
+from pyspark import SparkContext
+from csv import reader
+
+
+
+if __name__ == "__main__":
+	if len(sys.argv) != 2:
+		print("Usage: bigram <file>", file=sys.stderr)
+		exit(-1)
+	sc = SparkContext()
+	crime = sc.textFile(sys.argv[1], 1)
+	
+	crime = crime.mapPartitions(lambda x: reader(x)).map(lambda x: (x[18],1))
+	a=crime.reduceByKey(lambda x,y: x+y).sortBy(lambda x:x[1],ascending=False)
+	a=a.map(lambda x:(x[0]+str('\t')+str(x[1])))
+	a.saveAsTextFile('crime_18.out')
+	sc.stop()
---
+++ mr_final/crime_21.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+
+import sys
+from operator import add
+from pyspark import SparkContext
+from csv import reader
+
+
+
+if __name__ == "__main__":
+	if len(sys.argv) != 2:
+		print("Usage: bigram <file>", file=sys.stderr)
+		exit(-1)
+	sc = SparkContext()
+	crime = sc.textFile(sys.argv[1], 1)
+	
+	crime = crime.mapPartitions(lambda x: reader(x))\
+.map(lambda x: (x[21],1)).filter(lambda x: x[0]=='')
+	a=crime.reduceByKey(lambda x,y: x+y)
+	a=a.map(lambda x:x[0]+str('\t')+str(x[1]))
+	a.saveAsTextFile('crime_21.out')
+	sc.stop()
