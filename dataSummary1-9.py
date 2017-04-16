50d74127198e7e7c2aca01b8607ecde004520125
Zhiqi Guo <guozhiqi14@gmail.com>
2017-04-16 18:30:11 -0400

    modify summary

--- dataSummary1-9.py
+++ dataSummary1-9.py
@@ -36,22 +36,45 @@ crimedata = sc.textFile("/Users/guozhiqi-seven/Google Drive/NYU Master/Big Data/
 #crimedata.count() 
 
 CMPLNT_FR_DT_year = crimedata.mapPartitions(lambda x: reader(x)).map(lambda x: (x[1][-4:],1))
+CMPLNT_FR_DT_year.reduceByKey(add).coalesce(1).saveAsTextFile('CMPLNT_FR_DT_count_by_year')
 CMPLNT_FR_DT_year.reduceByKey(add).collect()
 
 CMPLNT_FR_DT_month = crimedata.mapPartitions(lambda x: reader(x)).map(lambda x: (x[1][:2],1))
+CMPLNT_FR_DT_month.reduceByKey(add).coalesce(1).saveAsTextFile('CMPLNT_FR_DT_count_by_month')
 CMPLNT_FR_DT_month.reduceByKey(add).collect()
 
 CMPLNT_FR_DT_date = crimedata.mapPartitions(lambda x: reader(x)).map(lambda x: (x[1][3:5],1))
+CMPLNT_FR_DT_date.reduceByKey(add).coalesce(1).saveAsTextFile('CMPLNT_FR_DT_count_by_date')
 CMPLNT_FR_DT_date.reduceByKey(add).collect()
 
 CMPLNT_FR_TM_hour = crimedata.mapPartitions(lambda x: reader(x)).map(lambda x: (x[2][:2],1))
+CMPLNT_FR_TM_hour.reduceByKey(add).coalesce(1).saveAsTextFile('CMPLNT_FR_TM_count_by_hour')
 CMPLNT_FR_TM_hour.reduceByKey(add).collect()
 
 CMPLNT_FR_TM_min = crimedata.mapPartitions(lambda x: reader(x)).map(lambda x: (x[2][3:5],1))
+CMPLNT_FR_TM_min.reduceByKey(add).coalesce(1).saveAsTextFile('CMPLNT_FR_TM_count_by_minute')
 CMPLNT_FR_TM_min.reduceByKey(add).collect()
 
+CMPLNT_TO_DT_year = crimedata.mapPartitions(lambda x: reader(x)).map(lambda x: (x[3][-4:],1))
+CMPLNT_TO_DT_year.reduceByKey(add).coalesce(1).saveAsTextFile('CMPLNT_TO_DT_count_by_year')
+
+CMPLNT_TO_DT_month = crimedata.mapPartitions(lambda x: reader(x)).map(lambda x: (x[3][:2],1))
+CMPLNT_TO_DT_month.reduceByKey(add).coalesce(1).saveAsTextFile('CMPLNT_TO_DT_count_by_month')
+
+CMPLNT_TO_DT_date = crimedata.mapPartitions(lambda x: reader(x)).map(lambda x: (x[3][3:5],1))
+CMPLNT_TO_DT_date.reduceByKey(add).coalesce(1).saveAsTextFile('CMPLNT_TO_DT_count_by_date')
+
+CMPLNT_TO_TM_hour = crimedata.mapPartitions(lambda x: reader(x)).map(lambda x: (x[4][:2],1))
+CMPLNT_TO_TM_hour.reduceByKey(add).coalesce(1).saveAsTextFile('CMPLNT_TO_TM_count_by_hour')
+
+CMPLNT_TO_TM_min = crimedata.mapPartitions(lambda x: reader(x)).map(lambda x: (x[4][3:5],1))
+CMPLNT_TO_TM_min.reduceByKey(add).coalesce(1).saveAsTextFile('CMPLNT_TO_TM_count_by_minute')
+
+RPT_DT_year = crimedata.mapPartitions(lambda x: reader(x)).map(lambda x: (x[5][-4:],1))
+RPT_DT_year.reduceByKey(add).coalesce(1).collect() 
 
 offense_classification = crimedata.mapPartitions(lambda x: reader(x)).map(lambda x: ((x[6],x[7]),1))
+offense_classification.reduceByKey(add).coalesce(1).saveAsTextFile('offense_classification')
 sorted(offense_classification.reduceByKey(add).collect(),key=lambda x:x[1],reverse=True)
 
 offense_classification = crimedata.mapPartitions(lambda x: reader(x)).map(lambda x: ((x[6],x[7]),1))
